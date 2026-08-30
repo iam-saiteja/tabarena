9603bba57330bd1f7ccf7fb4f13c2880338ced6f
Thanniru Sai Teja <iamsaitejathanniru@gmail.com>
2026-08-31 00:28:19 +0530

    feat(zstabfm): configure default manual config with 512 prototypes

--- packages/tabarena/src/tabarena/models/zstabfm/hpo.py
+++ packages/tabarena/src/tabarena/models/zstabfm/hpo.py
@@ -7,7 +7,6 @@ gen_zstabfm = ConfigGenerator(
     model_cls=ZSTabFMModel,
     manual_configs=[
         {"num_prototypes": 512, "interface": "default"},
-        {"num_prototypes": 1024, "interface": "ensemble"},
     ],
     search_space={
         "num_prototypes": [256, 512, 1024],
